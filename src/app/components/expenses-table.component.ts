d254a9f7c8b7bfd7c04b760a32057369d0bea14f
franbarreneche <franbarreneche@gmail.com>
2023-06-24 21:03:09 -0300

    update table component

--- src/app/components/expenses-table.component.ts
+++ src/app/components/expenses-table.component.ts
@@ -6,18 +6,57 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="card w-96 bg-neutral text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Cookies!</h2>
-        <p>We are using cookies for no reason.</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Accept</button>
-          <button class="btn btn-ghost">Deny</button>
+    <div class="card bg-white text-white-content shadow">
+      <div class="card-body items-center">
+
+        <div class="overflow-x-auto">
+          <table class="table">
+            <!-- head -->
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Detail</th>
+                <th>Amount</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr *ngFor="let expense of expenses" class="hover:bg-gray-100">
+                <th>{{ expense.date | date}}</th>
+                <td>{{ expense.detail  }}</td>
+                <td>{{ expense.price | currency:'USD':true }}</td>
+                <td><button class="btn btn-neutral btn-xs">delete</button></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
       </div>
     </div>
   `,
 })
 export class ExpensesTableComponent {
+  expenses = [
+    {
+      date: '2023-06-21',
+      detail: 'Panaderia',
+      price: 450,
+    },
+    {
+      date: '2023-06-22',
+      detail: 'Verdulería',
+      price: 1450,
+    },{
+      date: '2023-06-23',
+      detail: 'Supermercado',
+      price: 3274,
+    },
+    {
+      date: '2023-06-24',
+      detail: 'Paddel',
+      price: 950,
+    }
+
+  ];
 
 }
