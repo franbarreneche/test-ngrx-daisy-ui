2d35aa9e20ad412e2cf185974c67695bc7934913
franbarreneche <franbarreneche@gmail.com>
2023-06-24 21:03:00 -0300

    update kpis component

--- src/app/components/expenses-kpis.component.ts
+++ src/app/components/expenses-kpis.component.ts
@@ -6,18 +6,37 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="card w-96 bg-neutral text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Cookies!</h2>
-        <p>We are using cookies for no reason.</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Accept</button>
-          <button class="btn btn-ghost">Deny</button>
-        </div>
+        <div class="stats shadow">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </div>
+            <div class="stat-title">Total Expenses</div>
+            <div class="stat-value">{{ kpis.totalExpenses | currency:'USD':true }}</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+            </div>
+            <div class="stat-title">Mean Price</div>
+            <div class="stat-value">{{ kpis.meanPrice | currency:'USD':true }}</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+            </div>
+            <div class="stat-title">Median Price</div>
+            <div class="stat-value">{{ kpis.medianPrice | currency:'USD':true }}</div>
+          </div>
       </div>
-    </div>
   `,
 })
 export class ExpensesKpisComponent {
-
+  kpis = {
+    totalExpenses: 31000,
+    meanPrice: 4200,
+    medianPrice: 1200,
+  };
 }
