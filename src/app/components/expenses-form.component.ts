3684e44065b260a0ff1d0bbfedaf3d2b3281ae7c
franbarreneche <franbarreneche@gmail.com>
2023-06-24 23:27:41 -0300

    adds angular form to form component

--- src/app/components/expenses-form.component.ts
+++ src/app/components/expenses-form.component.ts
@@ -1,40 +1,63 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { expenseActions } from '../store/expenses/expenses.actions';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expenses-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <div class="card bg-white text-white-content self-center shadow">
       <div class="card-body items-center text-center">
         <h2 class="card-title">Add New Expense</h2>
+        <form [formGroup]="form" (submit)="onAdd()">
           <!-- Date -->
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Date</span>
-          </label>
-          <input type="date" placeholder="Supermarket" class="input input-bordered w-full max-w-xs" />
-        </div>
-        <!-- Detail -->
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Detail</span>
-          </label>
-          <input type="text" placeholder="Ex: Supermarket" class="input input-bordered w-full max-w-xs" />
-        </div>
-        <!-- Price -->
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Price</span>
-          </label>
-          <input type="number" placeholder="Ex: 150" class="input input-bordered w-full max-w-xs" />
-        </div>
-        <!-- Add -->
-        <button class="btn btn-block btn-neutral">Add</button>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Date</span>
+            </label>
+            <input formControlName="date" type="date" placeholder="Supermarket" class="input input-bordered w-full max-w-xs" />
+          </div>
+          <!-- Description -->
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Description</span>
+            </label>
+            <input formControlName="description" type="text" placeholder="Ex: Supermarket" class="input input-bordered w-full max-w-xs" />
+          </div>
+          <!-- Price -->
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Price</span>
+            </label>
+            <input formControlName="price" type="number" placeholder="Ex: 150" class="input input-bordered w-full max-w-xs" />
+          </div>
+          <!-- Add -->
+          <div class="mt-2">
+            <button class="btn btn-block btn-neutral" [disabled]="!form.valid">Add</button>
+          </div>
+        </form>
+      </div>
     </div>
   `,
 })
 export class ExpensesFormComponent {
+  protected form = this.fb.nonNullable.group({
+    date: ['', Validators.required],
+    description: ['', Validators.required],
+    price: [0, Validators.required],
+  });
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly store: Store
+  ) { }
+
+  onAdd() {
+    const value = this.form.getRawValue();
+    this.store.dispatch(expenseActions.addExpense(value));
+  }
 
 }
--- src/app/store/expenses/expenses.actions.ts
+++ src/app/store/expenses/expenses.actions.ts
@@ -4,7 +4,7 @@ import { Expense } from 'src/app/types/expense';
 export const expenseActions = createActionGroup({
   source: 'Expenses',
   events: {
-    'Add Expense': props<Expense>(),
+    'Add Expense': props<Omit<Expense, 'id'>>(),
     'Delete Expense': props<{ id: string }>(),
   },
 });
